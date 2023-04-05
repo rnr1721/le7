9d4afd6131b43f09b76905039bbda43bfc6af7ae
rnr1721 <admin@ehukr.com>
2023-04-05 19:05:35 +0300

    + able to overwrite response and template paths when get View from adapter

--- src/PhpViewAdapter.php
+++ src/PhpViewAdapter.php
@@ -10,6 +10,7 @@ use Core\Interfaces\ViewAdapter;
 use Core\Interfaces\ViewTopology;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\SimpleCache\CacheInterface;
 use Psr\Log\LoggerInterface;
 
@@ -18,7 +19,7 @@ class PhpViewAdapter implements ViewAdapter
 
     private CacheInterface $cache;
     private ServerRequestInterface $request;
-    private ResponseInterface $response;
+    private ResponseFactoryInterface $responseFactory;
     private WebPage $webPage;
     private ViewTopology $viewTopology;
     private LoggerInterface $logger;
@@ -27,7 +28,7 @@ class PhpViewAdapter implements ViewAdapter
             ViewTopology $viewTopology,
             WebPage $webPage,
             ServerRequestInterface $request,
-            ResponseInterface $response,
+            ResponseFactoryInterface $responseFactory,
             CacheInterface $cache,
             LoggerInterface $logger
     )
@@ -35,17 +36,27 @@ class PhpViewAdapter implements ViewAdapter
         $this->viewTopology = $viewTopology;
         $this->webPage = $webPage;
         $this->request = $request;
-        $this->response = $response;
+        $this->responseFactory = $responseFactory;
         $this->cache = $cache;
         $this->logger = $logger;
     }
 
-    public function getView(): View
+    public function getView(array|string|null $templatePath = null, ?ResponseInterface $response = null): View
     {
+
+        if ($response === null) {
+            $response = $this->responseFactory->createResponse(404);
+        }
+
+        if ($templatePath === null) {
+            $templatePath = $this->viewTopology->getTemplatePath();
+        }
+
         $template = new Template($this->logger);
-        $template->setPath($this->viewTopology->getTemplatePath());
+        $template->setPath($templatePath);
         $phpView = new PhpRenderEngine($template, $this->viewTopology);
-        return new PhpView($phpView, $this->webPage, $this->request, $this->response, $this->cache);
+
+        return new PhpView($phpView, $this->webPage, $this->request, $response, $this->cache);
     }
 
 }
